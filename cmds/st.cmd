dd5a0400172416f41310eb0da41b2dc599e163bc
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2021-05-25 11:02:09 +0200

    The very first test version.

---
+++ cmds/st.cmd
@@ -0,0 +1,22 @@
+require stream,2.8.10
+require smb100a,develop
+ 
+#Specify the TCP endpoint and give your 'bus' an arbitrary name eg. "asynstream1".
+drvAsynIPPortConfigure("PORT1","192.168.1.181:5025",0,0,0)
+
+#drvAsynSerialPortConfigure ("asynport2","/dev/ttyS0")
+#asynSetOption ("asynport2", 0, "baud", "9600")
+#asynSetOption ("asynport2", 0, "bits", "8")
+#asynSetOption ("asynport2", 0, "parity", "none")
+#asynSetOption ("asynport2", 0, "stop", "2")
+#asynSetOption ("asynport2", 0, "clocal", "N")
+#asynSetOption ("asynport2", 0, "crtscts", "N")
+
+#Indicate the folder where the MB100A protocol file is installed
+epicsEnvSet("STREAM_PROTOCOL_PATH","$(smb100a_DB)")
+
+#Load your database defining the EPICS records
+dbLoadRecords("smb100a.db", "PORT=PORT1,P=LLRF-1,DEV=LO")
+
+iocInit()
+
