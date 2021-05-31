9e7eb5b2e7910f74fff8ab7781a9d7ba05213fd6
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2021-05-31 11:44:48 +0200

    The first version for all generators.

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -4,6 +4,9 @@ require smb100a,0.1.0
 #Specify the TCP endpoint and give your 'bus' an arbitrary name eg. "asynstream1".
 drvAsynIPPortConfigure("PORT1","192.168.1.181:5025",0,0,0)
 drvAsynIPPortConfigure("PORT2","192.168.1.182:5025",0,0,0)
+drvAsynIPPortConfigure("PORT3","192.168.1.183:5025",0,0,0)
+drvAsynIPPortConfigure("PORT4","192.168.1.184:5025",0,0,0)
+drvAsynIPPortConfigure("PORT5","192.168.1.185:5025",0,0,0)
 
 #drvAsynSerialPortConfigure ("asynport2","/dev/ttyS0")
 #asynSetOption ("asynport2", 0, "baud", "9600")
@@ -17,8 +20,11 @@ drvAsynIPPortConfigure("PORT2","192.168.1.182:5025",0,0,0)
 epicsEnvSet("STREAM_PROTOCOL_PATH","$(smb100a_DB)")
 
 #Load your database defining the EPICS records
-dbLoadRecords("smb100a.db", "PORT=PORT1,P=LLRF-1,DEV=Ref")
-dbLoadRecords("smb100a.db", "PORT=PORT2,P=LLRF-2,DEV=Ref")
+dbLoadRecords("smb100a.db", "PORT=PORT1,P=LLRF-2,DEV=Ref")
+dbLoadRecords("smb100a.db", "PORT=PORT2,P=LLRF-1,DEV=Ref")
+dbLoadRecords("smb100a.db", "PORT=PORT3,P=LLRF-2,DEV=LO")
+dbLoadRecords("smb100a.db", "PORT=PORT4,P=LLRF-1,DEV=LO")
+dbLoadRecords("smb100a.db", "PORT=PORT5,P=LLRF,DEV=ADCTrig")
 
 iocInit()
 
